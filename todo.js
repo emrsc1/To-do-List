6480f8ddfdfaf29842f5d8768e40483cbd64f23f
Emre Aşcı <106581002+emrsc1@users.noreply.github.com>
2023-07-20 23:11:56 +0300

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,122 @@
+const form=document.querySelector("#todo-form");
+const todoInput=document.querySelector("#todo");
+const todoList=document.querySelector(".list-group");
+const firstCardBody=document.querySelectorAll(".card-body")[0];
+const secondCardBody=document.querySelectorAll(".card-body")[1];
+const filter=document.querySelector("#filter");
+const clearButton=document.querySelector("#clear-todos");
+eventListeners();
+function eventListeners(){
+  form.addEventListener("submit",addToDo);
+  document.addEventListener("DOMContentLoaded",loadAllTodosToUI);
+  secondCardBody.addEventListener("click",deleteTodo);
+  filter.addEventListener("keyup",filterTodos);
+  clearButton.addEventListener("click",clearAllTodos);
+}
+function clearAllTodos(e){
+  if(confirm("Tümünü silmek istediğinize emin misiniz?")){
+    while(todoList.firstElementChild!=null){
+      todoList.removeChild(todoList.firstElementChild);
+
+    }
+    localStorage.removeItem("todos");
+  }
+}
+function filterTodos(e){
+  const filterValue=e.target.value.toLowerCase();
+  const listItems=document.querySelectorAll(".list-group-item");
+
+  listItems.forEach(function(listItem){
+    const text=listItem.textContent.toLocaleLowerCase();
+    if(text.indexOf(filterValue)===-1){
+      listItem.setAttribute("style","display : none !important");
+    }
+    else{
+      listItem.setAttribute("style","display : block");
+    }
+  });
+}
+function deleteTodo(e){
+  if(e.target.className==="fa fa-remove"){
+    e.target.parentElement.parentElement.remove();
+    deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
+    showAlert("success","Todo başarıyla silindi...");
+  }
+}
+function loadAllTodosToUI(){
+  let todos=getTodosFromStorage();
+  todos.forEach(function(todo){
+    addTodoToUI(todo);
+  })
+}
+function deleteTodoFromStorage(deletetodo){
+  let todos=getTodosFromStorage();
+  todos.forEach(function(todo,index){
+    if(todo===deletetodo){
+      todos.splice(index,1);
+    }
+  });
+  localStorage.setItem("todos",JSON.stringify(todos));
+}
+function addToDo(e){
+    const newTodo =todoInput.value.trim();
+    listItems=document.querySelectorAll(".list-group-item");
+    let todoAll="";
+    listItems.forEach((listItem)=>{
+      todoAll+=listItem.textContent;
+    });
+    if(newTodo===""){
+      showAlert("danger","Lütfen bir To-do Giriniz.");
+    }
+    else{
+      if(newTodo!=todoAll){
+      addTodoToUI(newTodo);
+      addTodoToStorage(newTodo);
+      showAlert("success","Todo başarıyla eklendi.");
+      }
+      else{
+        showAlert("danger","Todo zaten mevcut.");
+      }
+    }
+    
+    
+    e.preventDefault();
+}
+function getTodosFromStorage(){
+  let todos;
+  if(localStorage.getItem("todos")===null){
+        todos=[];
+  }
+  else{
+    todos=JSON.parse(localStorage.getItem("todos"));
+  }
+  return todos;
+}
+function addTodoToStorage(NewTodo){
+   let todos=getTodosFromStorage();
+   todos.push(NewTodo);
+   localStorage.setItem("todos",JSON.stringify(todos));
+}
+function showAlert(type,message){
+  const alert=document.createElement("div");
+  alert.className='alert alert-'+type;
+  alert.textContent=message;
+  firstCardBody.appendChild(alert);
+  setTimeout(function(){
+    alert.remove();
+  },2000);
+}
+function addTodoToUI(newTodo){
+
+    const listItem=document.createElement("li");
+    const link=document.createElement("a");
+    link.href="#";
+    link.className="delete-item";
+    link.innerHTML="<i class = 'fa fa-remove'></i>";
+
+    listItem.className="list-group-item d-flex justify-content-between";
+    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(link);
+    todoList.appendChild(listItem);
+    todoInput.value="";
+}
